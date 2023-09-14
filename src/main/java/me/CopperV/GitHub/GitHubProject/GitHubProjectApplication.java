9589a584ec49dbed3273633444c41962daf5b03e
CopperV <vark123@interia.pl>
2023-09-14 17:14:13 +0200

    init

---
+++ src/main/java/me/CopperV/GitHub/GitHubProject/GitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package me.CopperV.GitHub.GitHubProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProjectApplication.class, args);
+	}
+
+}
